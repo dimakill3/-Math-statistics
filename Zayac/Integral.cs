9993238ea1e0e9bcb261a4c8d83b2331a41c6244
dimakill3 <VID-4@mail.ru>
2019-12-12 17:40:17 +0700

    + integral

---
+++ Zayac/Integral.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zayac
+{
+    class Integral
+    {
+        public delegate double Function(double x);
+
+        public static double Trapezoidal(Function f, double a, double b, int n)
+        {
+            double sum = 0.0;
+            double h = (b - a) / n;
+            for (int i = 0; i < n; i++)
+            {
+                sum += 0.5 * h * (f(a + i * h) + f(a + (i + 1) * h));
+            }
+            return sum;
+        }
+
+        static double laplas(double x)
+        {
+            x = Math.Pow(x, 2);
+            x /= -2;
+
+            return Math.Pow(Math.E, x);
+        }
+
+    }
+}
